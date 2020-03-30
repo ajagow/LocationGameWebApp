a2c3cce37309243cf6c814ea3e6ca3fec199a1fd
ajagow <budgiejag@gmail.com>
2020-03-30 15:42:32 -0400

    gatsby

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -30,5 +30,5 @@ export class MapContainer extends Component {
 }
 
 export default GoogleApiWrapper({
-  apiKey: (`${process.env.GOOGLE_API_KEY}`)
+  apiKey: (`${process.env.GATSBY_GOOGLE_API_KEY}`)
 })(MapContainer)
